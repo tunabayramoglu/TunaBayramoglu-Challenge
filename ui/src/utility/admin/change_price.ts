6c0926603fa7ac25aed6e94bc18d47c4dcecdfd7
tunabayramoglu <tunabayram34@gmail.com>
2025-10-15 21:43:36 +0300

    Complete all the TODOs

--- ui/src/utility/admin/change_price.ts
+++ ui/src/utility/admin/change_price.ts
@@ -11,7 +11,7 @@ export const changePrice = (packageId: string, listHeroId: string, newPriceInSui
   // TODO: Add moveCall to change hero price (Admin only)
   // Function: `${packageId}::marketplace::change_the_price`
   // Arguments: adminCapId (object), listHeroId (object), newPriceInMist (u64)
-  // Hints:
+  // Hints :
   // Use tx.object() for objects
   // Use tx.pure.u64() for the new price
   // Convert price from SUI to MIST before sending
